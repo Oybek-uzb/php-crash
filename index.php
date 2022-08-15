2fcfc601a81a05f9b5e064784c5905ed1c72bda1
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-08-15 12:34:01 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    echo "<button>Click me</button><br>", "Hello\n";
+    print "Hello by print\n";
+    print_r("awesome\n");
+    var_dump([10, 20, 30]);
+    var_export("<h1>Test</h1>");
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?= "<h1>Header 1</h1>" ?>
+</body>
+</html>
