f0b8832cafbc8d372a5fd480df7f3ef09ee1f9da
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-08-15 14:20:17 +0500

    variables and constants are done

---
+++ 02_variables.php
@@ -0,0 +1,31 @@
+<?php
+/* ------------------- PHP Data Types ------------------ */
+/*
+ * - String
+ * - Integer
+ * - Float
+ * - Boolean
+ * - Array
+ * - Object
+ * - NULL
+ * - Resource
+ */
+
+$name = "Ali";
+$floatNum = 11.11;
+
+echo $name;
+echo "\n";
+var_dump($floatNum);
+
+echo "\n name is " . $name . ' floatNum is ' . $floatNum;
+echo "\n";
+echo "My name is ${name} and my birthday in ${floatNum}";
+
+echo "\n";
+echo '5' + '5';
+
+const HOST = "localhost";
+const DB_NAME = "dev_db";
+
+echo HOST;
